43169ad149113351655af871022b73ea9227daf1
OtisLithebroke <82961023+OtisLithebroke@users.noreply.github.com>
2026-03-29 18:55:51 +0200

    asda

---
+++ app.py
@@ -0,0 +1,5 @@
+import strealit as st
+# this file is the main final file for the app, it will be used to run the app and will import the necessary functions from the other files
+
+
+st.write("Hello World")

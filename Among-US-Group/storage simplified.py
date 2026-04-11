e3976373d875dd8588a006b0a4749e73ebbd860d
aal3o-gif <spampam1980@gmail.com>
2026-04-11 19:31:07 +0200

    chanegs made

--- Among-US-Group/storage simplified.py
+++ Among-US-Group/storage simplified.py
@@ -1,8 +1,13 @@
-import json
-import os
+import json """JSON module allows us to convert a dictionary into a string and vice versa,
+which is useful for saving and loading data from a file - the same file that we access via 
+the OS importing."""
+
+import os """OS module allows us to interact with the operating system, 
+such as checking if a file exists or deleting a file."""
 
 # SAVE: Turn the dictionary into text and write it to a file
-seats = {"seat_A1": "free", "seat_A2": "occupied", "seat_A3": "free", "seat_A4": "occupied", "seat_A5": "free", "seat_A6": "occupied", "seat_A7": "free", "seat_A8": "occupied", "seat_A9": "free", "seat_A10": "occupied"}
+seats = {"seat_A1": "free", "seat_A2": "occupied", "seat_A3": "free", "seat_A4": "occupied",
+        "seat_A5": "free", "seat_A6": "occupied", "seat_A7": "free", "seat_A8": "occupied", "seat_A9": "free", "seat_A10": "occupied"}
 text = json.dumps(seats)
 file = open("seats.json", "w")
 file.write(text)

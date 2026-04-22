ea3a317e78bbfd6046a43520d7adb247d0081cc6
OtisLithebroke <82961023+OtisLithebroke@users.noreply.github.com>
2026-04-22 09:31:42 +0200

    Delete app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-import strealit as st
-# this file is the main final file for the app, it will be used to run the app and will import the necessary functions from the other files
-
-
-st.write("Hello World")
